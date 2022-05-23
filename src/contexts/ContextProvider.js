cbe7e4afb8089be2d61467115e6231746cd08512
ubong <ubongnt@gmail.com>
2022-05-23 18:13:04 +0100

    Correct  ContextProvider

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
@@ -11,28 +11,28 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [screenSize, setScreenSize] = useState(undefined);
-  const [currentColor, setCurrentColor] = useState("#03C9D7");
-  const [currentMode, setCurrentMode] = useState("Light");
+  const [currentColor, setCurrentColor] = useState('#03C9D7');
+  const [currentMode, setCurrentMode] = useState('Light');
   const [themeSettings, setThemeSettings] = useState(false);
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
 
   const setMode = (e) => {
     setCurrentMode(e.target.value);
-    localStorage.setItem("themeMode", e.target.value);
+    localStorage.setItem('themeMode', e.target.value);
   };
 
   const setColor = (color) => {
     setCurrentColor(color);
-    localStorage.setItem("colorMode", color);
+    localStorage.setItem('colorMode', color);
   };
 
-  const handleClick = (clicked) =>
-    setIsClicked({ ...initialState, [clicked]: true });
+  const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
 
   return (
     // eslint-disable-next-line react/jsx-no-constructed-context-values
     <StateContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
       value={{
         currentColor,
         currentMode,
