448a3c565822fbe943b066f950a5b96b7ea6462f
ubong <ubongnt@gmail.com>
2022-05-23 17:41:59 +0100

    correct

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban';
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from '@syncfusion/ej2-react-kanban';
 
 import { kanbanData, kanbanGrid } from '../data/dummy';
 import { Header } from '../components';
 
-const  Kanban = () => (
+const Kanban = () => (
   <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
     <Header category="App" title="Kanban" />
     <KanbanComponent
@@ -15,7 +19,10 @@ const  Kanban = () => (
     >
       <ColumnsDirective>
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        {kanbanGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        {kanbanGrid.map((item, index) => (
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          <ColumnDirective key={index} {...item} />
+        ))}
       </ColumnsDirective>
     </KanbanComponent>
   </div>
